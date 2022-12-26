ee973e11831ca4d8cca3dd0358c6ee598f8b38cb
Brayden Langley <brayden167@gmail.com>
2022-12-26 15:37:21 -0800

    Added support for hex strings, replaced rollup with webpack, and bundled src

--- src/encodeUint8AsString.js
+++ src/encodeUint8AsString.js
@@ -1,13 +1,16 @@
 /**
- * Given an arbitrary Uint8Array, returns a string that can later be decoddd with decodeUint8FromString
+ * Given an arbitrary Uint8Array, returns a string that can later be decoded with decodeUint8FromString
  */
 
 // Adapted from https://stackoverflow.com/a/36046727
-const encodeUint8AsString = uint8 => {
+const encodeUint8AsString = (uint8, format = 'base64') => {
   if (uint8.constructor !== Uint8Array) {
     throw new TypeError('The provided value is not a Uint8Array!')
   }
-  return Buffer.from(uint8).toString('base64')
+  if (format !== 'base64' && format !== 'hex') {
+    throw new TypeError('Output format must be a base64 or hex string!')
+  }
+  return Buffer.from(uint8).toString(format)
 }
 
 export default encodeUint8AsString
